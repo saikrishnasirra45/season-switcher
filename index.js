0e68798e563ae910d534d17f46c3c817ed4e9bb9
saikrishnasirra45 <saikrishnasirra@gmail.com>
2024-04-06 15:27:18 +0530

    feature signup working

---
+++ index.js
@@ -0,0 +1,17 @@
+let seasonSmallImageElement = document.getElementById("seasonSmallImage");
+
+function springButtonOne() {
+    seasonSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+}
+
+function summerButtonTwo() {
+    seasonSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+}
+
+function autumnButtonThree() {
+    seasonSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+}
+
+function winterButtonFour() {
+    seasonSmallImageElement.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+}
